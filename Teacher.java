0d852fd81a9daa3482082dad48d57eedcd1eb602
HermelaDemeke <bhmd0595@gmail.com>
2023-11-04 00:27:52 +0300

    this is the first project

---
+++ Teacher.java
@@ -0,0 +1,16 @@
+/*
+Create class Teacher with the following:
+Inherits from Person.
+ */
+public class Teacher extends Person{
+ String specialization;
+    //Constructor extends parent's constructor by adding specialization and a parameter for it.
+    public Teacher(String name, int age, boolean parent_permission) {
+        super(name, age, parent_permission);
+        this.specialization=specialization;
+    }
+    //Override can_use_services? so it always returns true.
+    public boolean can_use_service(){
+        return true;
+    }
+}
